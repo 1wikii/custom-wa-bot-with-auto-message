3cb075983b0403f068caf5c904b5d8bff0bce8ef
1wikii <ahmaddwikyzerrodixxon@gmail.com>
2025-07-17 16:34:58 +0700

    feat: add qr code generation and dependency

--- main.js
+++ main.js
@@ -19,7 +19,11 @@ const messages = [
 const randMsg = messages[Math.floor(Math.random() * 3)];
 
 client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
+  //   qrcode.generate(qr, { small: true });
+
+  const qrimg = require("qrcode");
+
+  qrimg.toFile("qr.png", qr, () => {});
 });
 
 client.on("ready", () => {
